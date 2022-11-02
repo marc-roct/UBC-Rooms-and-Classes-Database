efc9432528c3581d2a5ed47a66dcf3b9dad30698
enxie <enxie@students.cs.ubc.ca>
2022-11-01 23:30:39 -0700

    adding a new validator for transformations

--- src/controller/Utilities/queryValidator.ts
+++ src/controller/Utilities/queryValidator.ts
@@ -2,24 +2,27 @@ import {InsightError} from "../IInsightFacade";
 
 const queryValidator = (query: Record<string, any>): string => {
 	let databaseId: string;
-	let bodyTracker = 0;
+	let whereTracker = 0;
 	let optionTracker = 0;
+	let transformationsTracker = 0;
 	// get the keys from the query object
 	let keys = Object.keys(query);
 	keys.forEach((key) => {
 		if (key === "WHERE") {
-			bodyTracker += 1;
+			whereTracker += 1;
 		} else if (key === "OPTIONS") {
 			optionTracker += 1;
-		};
+		} else if (key === "TRANSFORMATIONS") {
+			transformationsTracker += 1;
+		}
 	});
-	if (bodyTracker === 0) {
+	if (whereTracker === 0) {
 		throw new InsightError("Missing WHERE");
 	};
 	if(optionTracker === 0) {
 		throw new InsightError("Missing OPTIONS");
 	};
-	if (bodyTracker > 1 || optionTracker > 1) {
+	if (whereTracker > 1 || optionTracker > 1) {
 		throw new InsightError("Invalid Query");
 	};
 
@@ -29,49 +32,54 @@ const queryValidator = (query: Record<string, any>): string => {
 		throw new InsightError("WHERE should only have 1 key, has 2");
 	};
 	let keyFields: string[] = [];
+	let columns = optionValidator(query["OPTIONS"], transformationsTracker);
 	keyFields = keyFields.concat(whereValidator(query["WHERE"]));
-	keyFields = keyFields.concat(optionValidator(query["OPTIONS"]));
+	keyFields = keyFields.concat(columns);
 	databaseId = checkDatasetReference(keyFields);
+
+	if(transformationsTracker !== 0) {
+		transformationsValidator(query["TRANSFORMATIONS"], columns);
+	}
 	return databaseId;
 };
 
-const whereValidator = (data: Record<string, any>): string[] => {
+const whereValidator = (whereClause: Record<string, any>): string[] => {
 	let keyFields: string[] = [];
-	if(typeof data === "object" && !Array.isArray(data)) {
+	if(typeof whereClause === "object" && !Array.isArray(whereClause)) {
 		// if the input data is an object: push the key to the dataCollector
 		// and call queryParser to check the values inside the object
-		for(let key in data) {
+		for(let key in whereClause) {
 			if(key === "OR" || key === "AND") {
-				logicValidator(data[key], key);
-				keyFields = keyFields.concat(whereValidator(data[key]));
+				logicValidator(whereClause[key], key);
+				keyFields = keyFields.concat(whereValidator(whereClause[key]));
 			} else if(key === "LT" || key === "GT" || key === "EQ") {
-				mComparisonValidator(data[key], key);
+				mComparisonValidator(whereClause[key], key);
 				// extract keys from the query
-				keyFields = keyFields.concat(Object.keys(data[key]));
+				keyFields = keyFields.concat(Object.keys(whereClause[key]));
 			} else if (key === "IS") {
-				sComparisonValidator(data[key], key);
-				keyFields = keyFields.concat(Object.keys(data[key]));
+				sComparisonValidator(whereClause[key], key);
+				keyFields = keyFields.concat(Object.keys(whereClause[key]));
 			} else if (key === "NOT") {
-				negationValidator(data[key], key);
+				negationValidator(whereClause[key], key);
 				// extract keys from the query
-				keyFields = keyFields.concat(whereValidator(data[key]));
+				keyFields = keyFields.concat(whereValidator(whereClause[key]));
 			} else {
 				throw new InsightError("Invalid query string");
 			};
 		}
-	} else if (Array.isArray(data)) {
+	} else if (Array.isArray(whereClause)) {
 		// if the input data is an array: loop through each item and call queryParser()
 		// only update the result when an error occurs
-		for(let index in data) {
-			keyFields = keyFields.concat(whereValidator(data[index]));
+		for(let index in whereClause) {
+			keyFields = keyFields.concat(whereValidator(whereClause[index]));
 		}
 	};
 	return keyFields;
 };
 
-const optionValidator = (options: Record<string, any>): string[] => {
+const optionValidator = (optionClause: Record<string, any>, transformationsTracker: number): string[] => {
 	let keyFields: string[] = [];
-	let keys = Object.keys(options);
+	let keys = Object.keys(optionClause);
 	if (keys.length > 2) {
 		throw new InsightError("Invalid Query - duplicate COLUMNS OR ORDER");
 	}
@@ -79,12 +87,12 @@ const optionValidator = (options: Record<string, any>): string[] => {
 	if(keys[0] !== "COLUMNS") {
 		throw new InsightError("OPTIONS missing COLUMNS");
 	} else {
-		columns = options["COLUMNS"];
+		columns = optionClause["COLUMNS"];
 		if(columns.length === 0) {
 			throw new InsightError("COLUMNS must be a non-empty array");
 		} else {
 			columns.forEach((column: string) => {
-				if(!mFieldValidator(column) && !sFieldValidator(column)) {
+				if(!mFieldValidator(column) && !sFieldValidator(column) && transformationsTracker === 0) {
 					throw new InsightError("Invalid key " + column + " in COLUMNS");
 				} else {
 					keyFields.push(column);
@@ -93,11 +101,12 @@ const optionValidator = (options: Record<string, any>): string[] => {
 		};
 	}
 
+	// TODO: update ORDER validator to perform new feature
 	if(keys.length === 2) {
 		if(keys[1] !== "ORDER") {
 			throw new InsightError("Invalid keys in OPTIONS");
 		} else {
-			let order = options["ORDER"];
+			let order = optionClause["ORDER"];
 			if(typeof order !== "string") {
 				throw new InsightError("Invalid ORDER type");
 			}else if(!columns.includes(order)) {
@@ -106,6 +115,23 @@ const optionValidator = (options: Record<string, any>): string[] => {
 		}
 	};
 	return keyFields;
+};
+
+// TODO: to test this new function
+const transformationsValidator = (transformations: Record<string, any>, applyKeyInColumns: string[]): void => {
+	let keyObjects = transformations["APPLY"];
+	let applyKeys: string [] = [];
+	keyObjects.forEach((keyObject: object) => {
+		let key = Object.keys(keyObject);
+		applyKeys.push(key[0]);
+	});
+	console.log("************ PRINTING OUT APPLY KEYS");
+	console.log(applyKeys);
+	applyKeys.forEach((key)=> {
+		if(!applyKeyInColumns.includes(key)) {
+			throw new InsightError("Invalid key " + key + " in APPLY");
+		}
+	});
 };
 
 const checkDatasetReference = (keyFields: string[]): string => {
