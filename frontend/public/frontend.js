b09ce505759ab2aa707f1e4d7c26272c7a308526
enxie <enxie@students.cs.ubc.ca>
2022-11-29 17:45:59 -0800

    draft the frontend

--- frontend/public/frontend.js
+++ frontend/public/frontend.js
@@ -1,5 +1,44 @@
-document.getElementById("click-me-button").addEventListener("click", handleClickMe);
+document.getElementById("add-dataset").addEventListener("click", handleAddDataset);
 
-function handleClickMe() {
-	alert("Button Clicked!");
+function handleAddDataset() {
+	let data = {mock: "testing"};
+	fetch('http://localhost:4321/echo/hello', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),
+	}).then(function (response) {
+		console.log("printing response:");
+		console.log(response);
+		return response.json();
+	}).then(function(data) {
+		console.log("printing data:");
+		console.log(data);
+	}).catch(function(err){
+		console.log('Error: ', err);
+	});
+	// alert("Add Dataset Button Clicked!");
+
 }
+
+document.getElementById("list-dataset").addEventListener("click", handleListDataset);
+
+function handleListDataset() {
+	let result = "data is not fetched";
+	fetch('http://localhost:4321/echo/hello')
+		.then(function (response) {
+			console.log("printing response:");
+			console.log(response);
+			return response.json();
+		}).then(function (data) {
+			result = data;
+			console.log("printing data:");
+			console.log(result);
+	}).catch(function(err) {
+		console.log(err);
+	})
+	// alert("List dataset Button Clicked!" + result);
+}
+
+
